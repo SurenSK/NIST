cbaa7140dfec1f89e6ea5624a6a9938674f5b965
Suren <kumar.s.suren@gmail.com>
2025-05-31 18:01:24 -0400

    squeue check

--- threads.py
+++ threads.py
@@ -1,9 +1,11 @@
 import os
+import subprocess
 from time import sleep
 
-my_id = int(os.environ['SLURM_ARRAY_TASK_ID'])
-total_count = int(os.environ['SLURM_ARRAY_TASK_COUNT'])
+result = subprocess.run(['squeue', '-u', 'skumar43', '-h', '-t', 'R'], capture_output=True, text=True)
+total_count = len(result.stdout.strip().split('\n')) if result.stdout.strip() else 0
+
+my_id = int(os.environ['SLURM_JOBID'])
 
 print(f"Seeing my_id={my_id}, total_count={total_count}")
-
 sleep(10)
