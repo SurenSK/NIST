8a7d56f48c2afbc6a573a0601f30b7c68fcc6f20
Suren <kumar.s.suren@gmail.com>
2025-05-31 17:34:15 -0400

    logging

--- threads.py
+++ threads.py
@@ -1,3 +1,9 @@
 import os
+from time import sleep
+
 my_id = int(os.environ['SLURM_ARRAY_TASK_ID'])
 total_count = int(os.environ['SLURM_ARRAY_TASK_COUNT'])
+
+print(f"Seeing my_id={my_id}, total_count={total_count}")
+
+sleep(10)
